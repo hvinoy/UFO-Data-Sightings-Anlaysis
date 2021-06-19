0ebdea8afd6aee640d876a98da9f76e46bb2923a
hvinoy <vinoyharsha@gmail.com>
2021-06-18 20:18:35 -0400

    updates

--- test.js
+++ test.js
@@ -4,9 +4,6 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-
-
-
 // adding data to the page from data.js
 tableData.forEach(function(ufodata) { //loop through each report in data
     console.log(ufodata);
@@ -19,7 +16,17 @@ tableData.forEach(function(ufodata) { //loop through each report in data
     });
 });
 
+function dataUpload() {
+    var row = tbody.append("tr");
+    Object.entries(tableData).forEach(function([key, value]) {  //get the key and values of each category in a ufo sighting
+    console.log(key, value);
+//Append a cell to the row for each value
+    var cell = row.append("td"); //append td to page to add the value
+    cell.text(value); //adds the value to the page
+    });
+});
 
+}
 
 //filtering data
 
@@ -55,20 +62,48 @@ function filter() {
 
 }
 
-// reset data
-var reset = d3.select("#reset-btn")
-reset.on("click",dataUpload)
+function compare(){
+    ufodata = tableData.datetime;
+    
+}
 
 
-function dataUpload() {
-    tbody.html("");
-    tableData.forEach(function(ufodata) { //loop through each report in data
-        console.log(ufodata);
-        var row = tbody.append("tr"); //append a tr attribute to create row for ufo values
-        Object.entries(ufodata).forEach(function([key, value]) {  //get the key and values of each category in a ufo sighting
-        console.log(key, value);
-        //Append a cell to the row for each value
-        var cell = row.append("td"); //append td to page to add the value
-        cell.text(value); //adds the value to the page
-        });
-    });
+// show full table again
+//needs to add if statement or another code
+
+function filter() {
+    d3.event.preventDefault()
+    var inputDate = d3.select("#datetime");
+    var inputValue = inputDate.property("value");
+    console.log(inputValue);
+
+    if (tableData.filter(ufodata => ufodata.datetime === inputValue)) {
+
+        var filteredData = tableData.filter(ufodata => ufodata.datetime === inputValue);
+        console.log(filteredData);
+    
+        tbody.html("");
+
+
+        filteredData.forEach(function(ufo) { //loop through each report in data
+            console.log(ufo);
+            var row = tbody.append("tr"); //append a tr attribute to create row for ufo values
+            Object.entries(ufo).forEach(function([key, value]) {  //get the key and values of each category in a ufo sighting
+            console.log(key, value);
+            //Append a cell to the row for each value
+            var cell = row.append("td"); //append td to page to add the value
+            cell.text(value); //adds the value to the page
+            });
+    });}
+
+
+    else if (tableData.filter ("" === inputvalue))
+        tbody.html("");
+        dataUpload(tableData)
+        console.log("works")
+    }
+}
+
+
+
+
